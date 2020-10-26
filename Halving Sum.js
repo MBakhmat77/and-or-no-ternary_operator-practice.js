cf8b7eb7ab32e975a8de059851bf2772c25125fc
Marina Bakhmat <48866406+MBakhmat77@users.noreply.github.com>
2020-10-26 17:01:01 -0400

    Create Halving Sum.js

---
+++ Halving Sum.js
@@ -0,0 +1,14 @@
+//Given a positive integer n, calculate the following sum:
+
+n + n/2 + n/4 + n/8 + ...
+All elements of the sum are the results of integer division.
+
+Example
+function halvingSum(n) {
+    let sum = 0;
+  while(n > 0 ){  
+  sum = sum + n;
+  n = Math.floor(n/2);
+  }
+  return sum;
+}
